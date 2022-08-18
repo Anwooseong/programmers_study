ca12359aaca244d6ae0f9aedd67a8d59cfbdb8ed
Anwooseong <aws5624@naver.com>
2022-08-18 13:35:48 +0900

    로봇 청소기(DFS구현 - 주의할점:후진할때 청소했어도 가능하다)

--- src/main/java/week2/Baekjoon14503.java
+++ src/main/java/week2/Baekjoon14503.java
@@ -5,52 +5,45 @@ import java.util.Scanner;
 public class Baekjoon14503 {
     static int[] dirX = {-1, 0, 1, 0};
     static int[] dirY = {0, 1, 0, -1};
-    static int[][] region;
+    static int[][] region, ch;
     static int answer, N, M;
 
 
     public void DFS(int r, int c, int d) {
-        if (region[r][c] == 0){
-            region[r][c] = 2; //청소했다는 의미
+        if (ch[r][c] == 0 && region[r][c] == 0) {
+            ch[r][c] = 1; //청소했다는 의미
             answer++;
         }
         int cnt = 0;
         int nextDirection = d;
         while (cnt != 4) {
-            nextDirection = directionClean(nextDirection);
-            int nextRow = r + dirX[nextDirection];
-            int nextColumn = c + dirY[nextDirection];
-            if (nextRow > 0 && nextRow < N && nextColumn > 0 && nextColumn < M) {
-                if (region[nextRow][nextColumn] == 0) {
-                    DFS(nextRow, nextColumn, nextDirection);
+
+            if (nextDirection == 0) nextDirection = 3;
+            else if (nextDirection == 1) nextDirection = 0;
+            else if (nextDirection == 2) nextDirection = 1;
+            else if (nextDirection == 3) nextDirection = 2;
+
+            if ((r + dirX[nextDirection]) > 0 && (r + dirX[nextDirection]) < N && (c + dirY[nextDirection]) > 0 && (c + dirY[nextDirection]) < M) {
+                if (region[(r + dirX[nextDirection])][(c + dirY[nextDirection])] == 0 && ch[(r + dirX[nextDirection])][(c + dirY[nextDirection])] == 0) {
+                    DFS((r + dirX[nextDirection]), (c + dirY[nextDirection]), nextDirection);
                     break;
                 }
             }
             cnt++;
         }
         if (cnt == 4) {
-            nextDirection = directionBack(d);
-            int nextRow = r + dirX[nextDirection];
-            int nextColumn = c + dirY[nextDirection];
-            if (nextRow > 0 && nextRow < N && nextColumn > 0 && nextColumn < M) {
-                if (region[nextRow][nextColumn] != 1) {
-                    DFS(nextRow, nextColumn, d);
+
+            if (nextDirection == 0) nextDirection = 2;
+            else if (nextDirection == 1) nextDirection = 3;
+            else if (nextDirection == 2) nextDirection = 0;
+            else if (nextDirection == 3) nextDirection = 1;
+
+            if ((r + dirX[nextDirection]) > 0 && (r + dirX[nextDirection]) < N && (c + dirY[nextDirection]) > 0 && (c + dirY[nextDirection]) < M) {
+                if (region[(r + dirX[nextDirection])][(c + dirY[nextDirection])] == 0) {
+                    DFS((r + dirX[nextDirection]), (c + dirY[nextDirection]), d);
                 }
             }
         }
-    }
-
-    public int directionBack(int dir) {
-        if (dir == 0) return 2;
-        else if (dir == 1) return 3;
-        else if (dir == 2) return 0;
-        else return 1;
-    }
-    public int directionClean(int dir) {
-        if (dir == 0) return 3;
-        else if (dir == 1) return 0;
-        else if (dir == 2) return 1;
-        else return 2;
     }
 
     public static void main(String[] args) {
@@ -62,6 +55,7 @@ public class Baekjoon14503 {
         int c = kb.nextInt();
         int d = kb.nextInt();
         region = new int[N][M];
+        ch = new int[N][M];
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 region[i][j] = kb.nextInt();
