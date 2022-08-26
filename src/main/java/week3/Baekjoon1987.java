15592ccfb669449dc46be376133e006fa1dd4f3f
Anwooseong <aws5624@naver.com>
2022-08-27 00:51:11 +0900

    알파벳(DFS구현 : 해쉬맵 탐색시간은 O(1)임을 이용)

--- src/main/java/week3/Baekjoon1987.java
+++ src/main/java/week3/Baekjoon1987.java
@@ -1,28 +1,30 @@
 package week3;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 //알파벳
 //https://www.acmicpc.net/problem/1987
 public class Baekjoon1987 {
     static int R, C;
-    static int[][] board;
-    static int[] alphabet = new int[26];
+    static char[][] board;
+    static Map<Character, Integer> alphabet = new HashMap<>();
     static int[] dirX = {0, 0, -1, 1};
     static int[] dirY = {-1, 1, 0, 0};
     static int answer = 0;
 
-    public void DFS(int index, int x, int y, int cnt) {
+    public void DFS(char key, int x, int y, int cnt) {
 
-        alphabet[index] = 1;
+        alphabet.put(key, alphabet.getOrDefault(key, 1));
         answer = Math.max(answer, cnt + 1);
 
         for (int i = 0; i < 4; i++) {
             int nextX = x + dirX[i];
             int nextY = y + dirY[i];
-            if (nextX >= 0 && nextY >= 0 && nextX < R && nextY < C && alphabet[board[nextX][nextY]] == 0) {
+            if (nextX >= 0 && nextY >= 0 && nextX < R && nextY < C && !alphabet.containsKey(board[nextX][nextY])) {
                 DFS(board[nextX][nextY], nextX, nextY, cnt + 1);
-                alphabet[board[nextX][nextY]] = 0;
+                alphabet.remove(board[nextX][nextY]);
             }
         }
     }
@@ -33,12 +35,12 @@ public class Baekjoon1987 {
         R = kb.nextInt();
         C = kb.nextInt();
         kb.nextLine();
-        board = new int[R][C];
+        board = new char[R][C];
 
         for (int i = 0; i < R; i++) {
             String str = kb.nextLine();
             for (int j = 0; j < C; j++) {
-                board[i][j] = str.charAt(j) - 'A';
+                board[i][j] = str.charAt(j);
             }
         }
 
