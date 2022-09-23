05b73f823b52f77830614387ec2e1a4de5b42ade
Anwooseong <aws5624@naver.com>
2022-09-24 04:46:48 +0900

    탑(스택) - 예전에 풀었던 6198번(옥상 정원 꾸미기) 문제와 매우 유사하다.

---
+++ src/main/java/week5/Baekjoon2493.java
@@ -0,0 +1,42 @@
+package week5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Baekjoon2493 {
+
+    static int N;
+    static int[] towers;
+    static Stack<Integer> stack;
+
+    public void solution() {
+        for (int i = 0; i < N; i++) {
+            while (!stack.isEmpty() && towers[stack.peek()] <= towers[i]) {
+                stack.pop();
+            }
+            if (stack.size() == 0) {
+                System.out.print(0 + " ");
+            } else {
+                System.out.print((stack.peek()+ 1)+" ");
+            }
+            stack.push(i);
+        }
+    }
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        Baekjoon2493 T = new Baekjoon2493();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        stack = new Stack<>();
+        towers = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            towers[i] = Integer.parseInt(st.nextToken());
+        }
+        T.solution();
+    }
+}
